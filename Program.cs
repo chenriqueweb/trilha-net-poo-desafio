0f678db4758f457b13fca0f1c6efd81b7144a741
chenriqueweb <carlos.henrique.mobile@gmail.com>
2023-01-27 19:30:11 -0300

    Desafio POO - Entrega

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.Clear();
+
+Console.WriteLine("Smartphone Noka:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "MOdelo 1", imei: "1212121212", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "MOdelo 1", imei: "1212121212", memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
